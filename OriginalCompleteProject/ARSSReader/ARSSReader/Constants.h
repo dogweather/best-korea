9dc18f3f2cd03d3e969dbdc0df705c2b1d1c8e67
Robb Shecter <robb@weblaws.org>
2013-03-26 07:20:24 -0700

    More placeholder images and optimized feed load times

--- OriginalCompleteProject/ARSSReader/ARSSReader/Constants.h
+++ OriginalCompleteProject/ARSSReader/ARSSReader/Constants.h
@@ -13,24 +13,18 @@
 // Best Korea Configuration
 #define REALITY                 @"Party-Pooper Mode"
 #define REALITY_APP_NAME        @"Best Korea"
-#define REALITY_NEWS_FEED       @"https://news.google.com/news/feeds?hl=en&gl=us&q=north+korea&um=1&ie=UTF-8&output=rss"
+#define REALITY_NEWS_FEED       @"http://bestkoreaapp.com/media/news/google.com.rss"
 #define REALITY_NAVBAR_FONT     @"Optima-ExtraBlack"
 
 #define ALTERNATE_REALITY       @"Party Mode"
 #define ALTERNATE_APP_NAME      @"BEST KOREA"
-#define ALTERNATE_NEWS_FEED     @"http://www.workers.org/tag/dprk/feed/"
+#define ALTERNATE_NEWS_FEED     @"http://bestkoreaapp.com/media/news/workers.org.rss"
 #define ALTERNATE_NAVBAR_FONT   @"Futura-CondensedExtraBold"
 
-#define ALTERNATE_PLACEHOLDER_ICONS 7
+#define ALTERNATE_PLACEHOLDER_ICONS 10
 
 
 // General News App Configuration
 #define PREF_ALTERNATE_REALITY  @"AlternateReality"
-
-// This is defined in Math.h
-#define M_PI   3.14159265358979323846264338327950288   /* pi */
-
-// Our conversion definition
-#define DEGREES_TO_RADIANS(angle) (angle / 180.0 * M_PI)
 
 #endif

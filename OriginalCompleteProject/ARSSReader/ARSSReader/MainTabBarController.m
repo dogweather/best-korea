68826b5fcd652e9279b9ec9699fe3118b679dcf9
Robb Shecter <robb@weblaws.org>
2013-03-26 05:39:20 -0700

    GOT IT.

--- OriginalCompleteProject/ARSSReader/ARSSReader/MainTabBarController.m
+++ OriginalCompleteProject/ARSSReader/ARSSReader/MainTabBarController.m
@@ -6,6 +6,8 @@
 //  Copyright (c) 2013 Srsly.co. All rights reserved.
 //
 
+#import <QuartzCore/QuartzCore.h>
+
 #import "Constants.h"
 #import "MainTabBarController.h"
 #import "MasterViewController.h"
@@ -60,8 +62,6 @@
                && ! [App inAlternateReality]) {
         [self setAlternateReality:YES];
     }
-    
-    [self fadeIn];
 }
 
 
@@ -79,25 +79,66 @@
 }
 
 
-- (void) fadeIn {
-//    [UIView beginAnimations:@"fadeIn" context:NULL];
-//    [UIView setAnimationDuration:2];
-//    [UIView setAnimationCurve:UIViewAnimationCurveEaseInOut];
-//    self.blackView.alpha = 0;
-//    [UIView commitAnimations];
-    if (self.spinner == nil)
-        self.spinner = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"spinner.png"]];
-    self.spinner.transform = CGAffineTransformMakeScale(0.1f, 0.1f);
-    [self.blackView addSubview:self.spinner];
-    
-    [UIView beginAnimations:nil context:nil];
-    [UIView setAnimationDuration:3];
-    [UIView setAnimationCurve:UIViewAnimationCurveLinear];
-    
-    self.spinner.transform = CGAffineTransformMakeScale(1.0f, 1.0f);
-    
-    [UIView commitAnimations];
+- (void) fadeInAndSpin {
+    if ([App inAlternateReality])
+        [self spinUp];
+    else
+        [self fadeInLong];
+}
 
+
+- (void) spinUp {
+    if (self.spinner == nil) {
+        self.spinner = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"spinner.png"]];
+        self.spinner.center = self.view.center;
+    }
+    self.spinner.transform = CGAffineTransformMakeScale(0.01f, 0.01f);
+    self.spinner.alpha = 0;
+    [self.blackView addSubview:self.spinner];
+
+    CABasicAnimation *makeBiggerAnim=[CABasicAnimation animationWithKeyPath:@"transform.scale"];
+    makeBiggerAnim.fromValue=[NSNumber numberWithDouble:0.05];
+    makeBiggerAnim.toValue=[NSNumber numberWithDouble:1.0];
+    
+    CABasicAnimation *fadeAnim=[CABasicAnimation animationWithKeyPath:@"opacity"];
+    fadeAnim.fromValue=[NSNumber numberWithDouble:0.1];
+    fadeAnim.toValue=[NSNumber numberWithDouble:1.0];
+    
+    CABasicAnimation *rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+    rotationAnimation.toValue = [NSNumber numberWithFloat: M_PI * 2.0 /* full rotation*/ * 3.0 * 3.0 ];
+    rotationAnimation.cumulative = YES;
+    
+    CAAnimationGroup *group = [CAAnimationGroup animation];
+    group.duration = 1.5;
+    group.repeatCount = 1;
+    group.autoreverses = YES;
+    group.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    group.animations = @[makeBiggerAnim, rotationAnimation, fadeAnim];
+    group.delegate = self;
+    [self.spinner.layer addAnimation:group forKey:@"allMyAnimations"];
+}
+
+
+- (void) animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
+    [self fadeIn];
+}
+
+
+- (void) fadeIn {
+    [UIView beginAnimations:@"fadeIn" context:NULL];
+    [UIView setAnimationDuration:1];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+    self.blackView.alpha = 0;
+    [UIView commitAnimations];
+}
+
+
+- (void) fadeInLong {
+    [UIView beginAnimations:@"fadeIn" context:NULL];
+    [UIView setAnimationDuration:2.5];
+    [UIView setAnimationCurve:UIViewAnimationCurveEaseOut];
+    self.blackView.alpha = 0;
+    [UIView commitAnimations];
 }
 
 
@@ -111,6 +152,8 @@
     for (id v in self.viewControllers) {
         [self sendUpdateMessageTo:v];
     }
+    
+    [self fadeInAndSpin];
 }
 
 

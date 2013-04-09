417b738edd5d893c8bdb39dad8da58f37c25f5e5
Robb Shecter <robb@weblaws.org>
2013-04-09 01:55:07 -0700

    New party mode placeholder icons

--- OriginalCompleteProject/ARSSReader/ARSSReader/Constants.h
+++ OriginalCompleteProject/ARSSReader/ARSSReader/Constants.h
@@ -21,7 +21,7 @@
 #define ALTERNATE_NEWS_FEED     @"http://bestkoreaapp.com/media/news/workers.org.rss"
 #define ALTERNATE_NAVBAR_FONT   @"Futura-CondensedExtraBold"
 
-#define ALTERNATE_PLACEHOLDER_ICONS 10
+#define ALTERNATE_PLACEHOLDER_ICONS 12
 #define MARKETING_DN            @"BestKoreaApp.com"
 
 

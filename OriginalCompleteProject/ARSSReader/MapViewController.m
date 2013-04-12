1ba9889617391001a3c57246582f1fe80568c54a
Robb Shecter <robb@weblaws.org>
2013-04-11 18:59:33 -0700

    Checkpoint
    I haven't been able to get the map non-pixelated. I realized that I
    haven't made a "retina" version of each map.

--- OriginalCompleteProject/ARSSReader/MapViewController.m
+++ OriginalCompleteProject/ARSSReader/MapViewController.m
@@ -12,7 +12,6 @@
 {
     UIImageView *imageView;
 }
-
 @end
 
 
@@ -34,8 +33,8 @@
     [self.scrollView setCanCancelContentTouches:NO];
     self.scrollView.contentSize         = CGSizeMake(imageView.frame.size.width, imageView.frame.size.height);
     self.scrollView.contentInset        = UIEdgeInsetsMake(edge, edge, edge, edge);
-    self.scrollView.minimumZoomScale    = 0.7;
-    self.scrollView.maximumZoomScale    = 1.1;
+    self.scrollView.minimumZoomScale    = 0.1;
+    self.scrollView.maximumZoomScale    = 1.0;
     self.scrollView.delegate            = self;
     self.scrollView.backgroundColor     = [UIColor blackColor];
     
